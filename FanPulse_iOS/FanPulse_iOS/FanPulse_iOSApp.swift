91f280ecf7e0e7b665dc98ef37c806c2d12de735
Song Kim <nadana0929@gmail.com>
2025-12-16 10:40:43 +0900

    iOS 프로젝트 파일추가

---
+++ FanPulse_iOS/FanPulse_iOS/FanPulse_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanPulse_iOSApp.swift
+//  FanPulse_iOS
+//
+//  Created by 김송 on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FanPulse_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
