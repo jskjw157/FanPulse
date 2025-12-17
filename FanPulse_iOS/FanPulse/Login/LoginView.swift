89fbe61458b8407a3d86403e34972ce9c5cb6d0b
SONG <nadana0929@gmail.com>
2025-12-17 23:41:07 +0900

    로그인 타이틀 추가 + 흰색 카드 추가

--- FanPulse_iOS/FanPulse/Login/LoginView.swift
+++ FanPulse_iOS/FanPulse/Login/LoginView.swift
@@ -17,6 +17,33 @@ struct LoginView: View {
                 endPoint: UnitPoint(x: 1, y: 0.6)
             )
             .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                Spacer()
+                    .frame(height: 80)
+                
+                VStack(spacing: 8) {
+                    Text("Welcome to FanPulse")
+                        .font(.system(size: 28, weight: .bold))
+                        .foregroundColor(.white)
+                    
+                    Text("글로벌 K-POP 팬들의 인터랙티브 플랫폼")
+                        .font(.system(size: 14))
+                        .foregroundColor(.white.opacity(0.9))
+                }
+                .padding(.bottom, 40)
+                
+                VStack {
+                    // TODO: 로그인  . . .
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 450)
+                .background(Color.white)
+                .cornerRadius(30)
+                .padding(.horizontal, 24)
+                
+                Spacer()
+            }
         }
     }
 }
