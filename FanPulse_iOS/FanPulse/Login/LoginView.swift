61da41069b3349d9407d44ecc2ed1fc62ac28041
SONG <nadana0929@gmail.com>
2025-12-17 23:39:07 +0900

    배경 그라데이션 뷰 적용

--- FanPulse_iOS/FanPulse/Login/LoginView.swift
+++ FanPulse_iOS/FanPulse/Login/LoginView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            FadeToBackgroundView(
+                backgroundColor: Color(hex: "#EC4899"),
+                fadeColor: Color(hex: "#7E22CE"),
+                startPoint: UnitPoint(x: 0, y: 0.4),
+                endPoint: UnitPoint(x: 1, y: 0.6)
+            )
+            .ignoresSafeArea()
+        }
     }
 }
 
