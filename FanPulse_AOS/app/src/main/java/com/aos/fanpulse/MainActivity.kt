39def250cf8f2536aca1cd81d690dc3bcb113d3f
layuseong <layus1997@gmail.com>
2025-12-18 22:37:09 +0900

    로그인 화면 UI 구성중

--- FanPulse_AOS/app/src/main/java/com/aos/fanpulse/MainActivity.kt
+++ FanPulse_AOS/app/src/main/java/com/aos/fanpulse/MainActivity.kt
@@ -20,28 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             FanPulseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FanPulseTheme {
-        Greeting("Android")
     }
 }
