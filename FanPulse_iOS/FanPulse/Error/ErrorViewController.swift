6f682a8422f730d6742fa4b74e06c7b8b3447180
SONG <nadana0929@gmail.com>
2026-01-10 23:49:59 +0900

    feat(iOS): 에러화면 추가

---
+++ FanPulse_iOS/FanPulse/Error/ErrorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorViewController.swift
+//  FanPulse
+//
+//  Created by 김송 on 1/10/26.
+//
+
+import UIKit
+
+final class ErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "error"
+    }
+}
